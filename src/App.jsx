22305df7cdf07be58ed3df7c2271441679f89047
Pradeep-S28 <emails2pradeep1506@gmail.com>
2026-06-13 01:07:00 +0530

    Start Project

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <h1>Employee Management Dashboard</h1>
+    </>
+  );
 }
 
 export default App;
